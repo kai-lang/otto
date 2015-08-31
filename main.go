a6505fbd8baa80d71eac6876d55af62569e558bc
Connor Taffe <cpaynetaffe@gmail.com>
2015-08-31 16:16:05 -0500

    update

--- main.go
+++ main.go
@@ -5,14 +5,28 @@ import (
 	"bufio"
 	"unicode"
 	"fmt"
+	"strconv"
 )
 
 type TokenType int
 
 const (
-	KeywordToken TokenType = iota
+	ErrorToken TokenType = iota
+	KeywordToken
 	IdentToken
 	IntegerToken
+)
+
+type Keyword int
+
+const (
+	VarKeyword Keyword = iota // var
+	AssignKeyword // =
+	AddKeyword // +
+	SubKeyword // -
+	MulKeyword // *
+	ModKeyword // %
+	SemiColonKeyword // ;
 )
 
 type Token struct {
@@ -28,7 +42,7 @@ type Lexer struct {
 	buffer string
 	lastRuneWidth int
 	Line, Column int
-	currentState StateFunc
+	State StateFunc
 	tokens []*Token
 }
 
@@ -55,6 +69,39 @@ func (l *Lexer) Next() (rune, error) {
 	return r, nil
 }
 
+type RunePredicate func (rune) bool
+
+// Reads up to unaccepted rune
+func (l *Lexer) NextUpTo(pred RunePredicate) (r rune, err error) {
+	for {
+		r, err = l.Peek()
+		if err != nil || !pred(r) {
+			return r, err
+		}
+		l.Next()
+	}
+}
+
+// Ignores up to unaccepted rune
+func (l *Lexer) IgnoreUpTo(pred RunePredicate) (r rune, err error) {
+	for {
+		r, err = l.Peek()
+		if err != nil || !pred(r) {
+			return r, err
+		}
+		l.Ignore()
+	}
+}
+
+func (l *Lexer) Ignore() (rune, error) {
+	r, err := l.Next()
+	if err != nil {
+		return r, err
+	}
+	l.buffer = l.buffer[:len(l.buffer)-l.lastRuneWidth]
+	return r, nil
+}
+
 func (l *Lexer) Back() error {
 	err := l.Input.UnreadRune()
 	if err != nil {
@@ -76,34 +123,177 @@ func (l *Lexer) Peek() (rune, error) {
 	return r, nil
 }
 
+func (l *Lexer) emitToken(t *Token) {
+	l.tokens = append(l.tokens, t)
+}
+
 // utility whitespace lex function
-func (l *Lexer) eatWs() {
-	r, _ := l.Peek()
+func (l *Lexer) whitespace() error {
+	r, err := l.Peek()
+	if err != nil {
+		return err
+	}
 	for unicode.Is(unicode.White_Space, r) {
 		l.Next()
-		r, _ = l.Peek()
+		r, err = l.Peek()
+		if err != nil {
+			return err
+		}
 	}
 	l.Emit() // dump
+	return nil
+}
+
+func floatState(l *Lexer) StateFunc {
+	return nil
+}
+
+func numberState(l *Lexer) StateFunc {
+	base := 10
+
+	// possible prefix
+	r, err := l.Peek()
+	if err != nil {
+		return nil
+	}
+	if r == '0' {
+		l.Next()
+		r, err := l.Next()
+		if err != nil {
+			return nil
+		}
+		switch r {
+		case 'x':
+			l.Emit()
+			base = 16 // hexadecimal
+		case 'c':
+			l.Emit()
+			base = 8 // octal
+		case 'b':
+			l.Emit()
+			base = 2 // binary
+		default:
+			l.Back()
+		}
+	}
+
+	r, _ = l.NextUpTo(func(r rune) bool {
+		// allow '_' separator
+		if base == 16 {
+			return unicode.Is(unicode.Hex_Digit, r) || r == '_'
+		} else {
+			return unicode.IsDigit(r) || r == '_'
+		}
+	})
+
+	if r == '.' {
+		// floating point number
+		return floatState
+	} else {
+		i, err := strconv.ParseInt(l.Emit(), base, 64)
+		if err != nil {
+			l.emitToken(&Token {
+				Type: ErrorToken,
+				Payload: fmt.Sprint(err),
+				Line: l.Line,
+				Column: l.Column,
+			})
+			return startState
+		}
+		l.emitToken(&Token {
+			Type: IntegerToken,
+			Payload: i,
+			Line: l.Line,
+			Column: l.Column,
+		})
+		return expressionState
+	}
+}
+
+func expressionState(l *Lexer) StateFunc {
+	if l.whitespace() != nil {
+		return nil
+	}
+	r, err := l.Peek()
+	if err != nil {
+		return nil
+	}
+	switch {
+	case unicode.IsDigit(r):
+		return numberState
+	case r == '+' || r == '-' || r == '*' || r == '%':
+		return nil
+	case r == ';':
+		l.Next()
+		l.emitToken(&Token {
+			Type: KeywordToken,
+			Payload: SemiColonKeyword,
+			Line: l.Line,
+			Column: l.Column,
+		})
+		return startState
+	default:
+		l.Next()
+		l.emitToken(&Token {
+			Type: ErrorToken,
+			Payload: fmt.Sprintf("Unexpected rune '%c' in expression", r),
+			Line: l.Line,
+			Column: l.Column,
+		})
+		return startState
+	}
+}
+
+// = 1+3*a
+func assignExpressionState(l *Lexer) StateFunc {
+	if l.whitespace() != nil {
+		return nil
+	}
+	r, err := l.Ignore()
+	if err != nil {
+		return nil
+	}
+	if r != '=' {
+		// error, missing equals sign
+		l.emitToken(&Token {
+			Type: ErrorToken,
+			Payload: fmt.Sprintf("Expected '=', found %c\n", r),
+			Line: l.Line,
+			Column: l.Column,
+		})
+		return startState
+	} else {
+		l.emitToken(&Token {
+			Type: KeywordToken,
+			Payload: AssignKeyword,
+			Line: l.Line,
+			Column: l.Column,
+		})
+		return expressionState
+	}
 }
 
 func varIdentState(l *Lexer) StateFunc {
-	r, _ := l.Peek()
-	for unicode.IsLetter(r) || unicode.IsDigit(r) || r == '_' {
-		l.Next()
-		r, _ = l.Peek()
-	}
-	l.tokens = append(l.tokens, &Token {
+	l.NextUpTo(func (r rune) bool {
+		return unicode.IsLetter(r) || unicode.IsDigit(r) || r == '_'
+	})
+	l.emitToken(&Token {
 		Type: IdentToken,
 		Payload: l.Emit(),
 		Line: l.Line,
 		Column: l.Column,
 	})
-	return nil
+	return assignExpressionState
 }
 
 func varState(l *Lexer) StateFunc {
-	l.eatWs()
-	r, _ := l.Peek()
+	if l.whitespace() != nil {
+		return nil
+	}
+	r, err := l.Peek()
+	if err != nil {
+		return nil
+	}
 	if unicode.IsLetter(r) || r == '_' {
 		return varIdentState
 	}
@@ -111,27 +301,27 @@ func varState(l *Lexer) StateFunc {
 }
 
 func keywordState(l *Lexer) StateFunc {
-	r, _ := l.Peek()
-	for unicode.IsLetter(r) {
-		l.Next()
-		r, _ = l.Peek()
-	}
-	str := l.Emit()
-	l.tokens = append(l.tokens, &Token {
+	l.NextUpTo(unicode.IsLetter)
+	l.emitToken(&Token {
 		Type: KeywordToken,
-		Payload: str,
+		Payload: VarKeyword,
 		Line: l.Line,
 		Column: l.Column,
 	})
-	if str == "var" {
+	if l.Emit() == "var" {
 		return varState
 	}
 	return nil
 }
 
 func startState(l *Lexer) StateFunc {
-	l.eatWs()
-	r, _ := l.Peek()
+	if l.whitespace() != nil {
+		return nil
+	}
+	r, err := l.Peek()
+	if err != nil {
+		return nil
+	}
 	if unicode.IsLetter(r) {
 		return keywordState
 	}
@@ -139,8 +329,8 @@ func startState(l *Lexer) StateFunc {
 }
 
 func (l *Lexer) NextToken() *Token {
-	for l.currentState != nil {
-		l.currentState = l.currentState(l)
+	for l.State != nil {
+		l.State = l.State(l)
 		if len(l.tokens) > 0 {
 			t := l.tokens[len(l.tokens)-1]
 			l.tokens = l.tokens[:len(l.tokens)-1]
@@ -150,9 +340,57 @@ func (l *Lexer) NextToken() *Token {
 	return nil
 }
 
-func main() {
-	l := Lexer { currentState: startState, Input: bufio.NewReader(os.Stdin) }
-	for t := l.NextToken(); t != nil; t = l.NextToken() {
-		fmt.Println(t)
+// Prompting Reader
+type Repl struct {
+	Input *bufio.Reader
+	Output *bufio.Writer
+	Prompt string
+	lexer *Lexer
+}
+
+func (r *Repl) prompt() {
+	defer r.Output.Flush()
+	fmt.Fprint(r.Output, r.Prompt)
+}
+
+func (r *Repl) Init() {
+	r.lexer = &Lexer {
+		State: startState,
+		Input: bufio.NewReader(r),
 	}
+	r.prompt()
+}
+
+func (r *Repl) Read(p []byte) (n int, err error) {
+	for i := 0; i < len(p); i++ {
+		b, err := r.Input.ReadByte()
+		if err != nil {
+			return i, err
+		}
+		p[i] = b
+		if b == '\n' {
+			r.prompt()
+			return i, nil
+		}
+	}
+	return len(p), nil
+}
+
+func (r *Repl) NextToken() *Token {
+	return r.lexer.NextToken()
+}
+
+func main() {
+	r := Repl {
+		Input: bufio.NewReader(os.Stdin),
+		Output: bufio.NewWriter(os.Stdout),
+		Prompt: ">>> ",
+	}
+	r.Init()
+	for t := r.NextToken(); t != nil; t = r.NextToken() {
+		if t.Type == ErrorToken {
+			fmt.Println(t.Payload)
+		}
+	}
+	fmt.Printf("\n")
 }
